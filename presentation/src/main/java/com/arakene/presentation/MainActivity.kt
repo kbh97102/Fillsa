1b6991d3c24207736950d9f9242320ec49fe4650
arakene <arakene_dev@naver.com>
2025-05-27 09:22:43 +0900

    feat: 스플래시 권한검사 추가

--- presentation/src/main/java/com/arakene/presentation/MainActivity.kt
+++ presentation/src/main/java/com/arakene/presentation/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.arakene.presentation
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.READ_MEDIA_IMAGES
+import android.Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -10,13 +15,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.arakene.presentation.ui.theme.FillsaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
+        val splash = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        checkPermissionInSplash(splash)
+
         setContent {
             FillsaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -26,6 +35,25 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
+        }
+    }
+
+    private fun checkPermissionInSplash(splash: SplashScreen) {
+        var allPermissionNotAllowed = true
+
+        splash.setKeepOnScreenCondition({ allPermissionNotAllowed })
+
+        val requestPermissions =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { results ->
+                allPermissionNotAllowed = false
+            }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            requestPermissions.launch(arrayOf(READ_MEDIA_IMAGES, READ_MEDIA_VISUAL_USER_SELECTED))
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermissions.launch(arrayOf(READ_MEDIA_IMAGES))
+        } else {
+            requestPermissions.launch(arrayOf(READ_EXTERNAL_STORAGE))
         }
     }
 }
