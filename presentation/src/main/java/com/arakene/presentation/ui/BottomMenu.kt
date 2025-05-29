9746d4938b4625e0af4bac72b7a41e8afb62839f
arakene <arakene_dev@naver.com>
2025-05-29 12:54:28 +0900

    feat: 바텀메뉴 디자인 수정

--- presentation/src/main/java/com/arakene/presentation/ui/BottomMenu.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/BottomMenu.kt
@@ -1,24 +1,25 @@
 package com.arakene.presentation.ui
 
-import android.util.Log
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.arakene.presentation.R
+import com.arakene.presentation.util.Screens
 
 @Composable
 fun BottomNavigationBar(navController: NavHostController) {
     val items = listOf(
-        Pair("home", R.drawable.icn_copy),
-        Pair("list", R.drawable.icn_copy),
-        Pair("calendar", R.drawable.icn_copy),
-        Pair("my page", R.drawable.icn_copy),
+        Pair(Screens.Home, R.drawable.icn_bottom_menu_home),
+        Pair(Screens.List, R.drawable.icn_bottom_menu_list),
+        Pair(Screens.Calendar, R.drawable.icn_bottom_menu_calendar),
+        Pair(Screens.MyPage, R.drawable.icn_bottom_menu_my_page),
     )
 
 
@@ -28,22 +29,20 @@ fun BottomNavigationBar(navController: NavHostController) {
         val currentRoute = navBackStackEntry?.destination?.route
 
         items.forEach { item ->
+            val routeString = remember { item.first::class.qualifiedName }
             NavigationBarItem(
-                selected = currentRoute == "item.route",
+                selected = currentRoute == routeString,
                 onClick = {
-
-                    Log.e(">>>>", "route? ${currentRoute} item ${item}")
-
-                    if (currentRoute != "item.route") {
-//                        navController.navigate(item.route) {
-//                            popUpTo(navController.graph.startDestinationId) { saveState = true }
-//                            launchSingleTop = true
-//                            restoreState = true
-//                        }
+                    if (currentRoute != routeString) {
+                        navController.navigate(item.first) {
+                            popUpTo(navController.graph.startDestinationId) { saveState = true }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
                     }
                 },
                 icon = { Icon(painterResource(item.second), contentDescription = null) },
-                label = { Text("label") }
+                label = { Text(item.first.routeString) }
             )
         }
     }
