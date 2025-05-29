0edf183843ce777af7c518f1e95b72019366b772
arakene <arakene_dev@naver.com>
2025-05-29 21:11:51 +0900

    feat: 오늘 명언 연동

--- presentation/src/main/java/com/arakene/presentation/ui/home/HomeView.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/home/HomeView.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -17,15 +18,44 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.arakene.presentation.ui.theme.FillsaTheme
 import com.arakene.presentation.ui.theme.ImageSection
 import com.arakene.presentation.util.LocaleType
+import com.arakene.presentation.viewmodel.HomeViewModel
 
 @Composable
-fun HomeView() {
+fun HomeView(
+    viewModel: HomeViewModel = hiltViewModel()
+) {
+
+    LaunchedEffect(Unit) {
+        viewModel.getDailyQuotaNoToken("2025-05-29")
+    }
+
 
     var selectedLocale by remember {
         mutableStateOf(LocaleType.KOR)
+    }
+
+    val quota by remember(viewModel.currentQuota, selectedLocale) {
+        mutableStateOf(
+            if (selectedLocale == LocaleType.KOR) {
+                viewModel.currentQuota.korQuote ?: ""
+            } else {
+                viewModel.currentQuota.engQuote ?: ""
+            }
+        )
+    }
+
+    val author by remember(viewModel.currentQuota, selectedLocale) {
+        mutableStateOf(
+            if (selectedLocale == LocaleType.KOR) {
+                viewModel.currentQuota.korAuthor ?: ""
+            } else {
+                viewModel.currentQuota.engAuthor ?: ""
+            }
+        )
     }
 
     Column(
@@ -63,8 +93,8 @@ fun HomeView() {
         }
 
         WiseSayingSection(
-            text = "테스트 테스트",
-            author = "Ara",
+            text = quota,
+            author = author,
             next = {},
             before = {},
             modifier = Modifier.padding(top = 20.dp)
--- presentation/src/main/java/com/arakene/presentation/viewmodel/HomeViewModel.kt
+++ presentation/src/main/java/com/arakene/presentation/viewmodel/HomeViewModel.kt
@@ -1,9 +1,16 @@
 package com.arakene.presentation.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewModelScope
+import com.arakene.domain.responses.DailyQuoteDto
 import com.arakene.domain.usecase.GetDailyQuotaNoTokenUseCase
 import com.arakene.presentation.util.Action
 import com.arakene.presentation.util.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.time.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,10 +18,27 @@ class HomeViewModel @Inject constructor(
     private val getDailyQuotaNoTokenUseCase: GetDailyQuotaNoTokenUseCase
 ): BaseViewModel() {
 
+    val date = mutableStateOf(LocalDate.now())
+
+    var currentQuota by mutableStateOf(DailyQuoteDto())
 
     override fun handleAction(action: Action) {
         TODO("Not yet implemented")
     }
 
+    fun getDailyQuotaNoToken(date: String) = viewModelScope.launch {
+        getResponse(getDailyQuotaNoTokenUseCase(date))?.let {
+            currentQuota = DailyQuoteDto(
+                likeYn = "N",
+                imagePath = "",
+                dailyQuoteSeq = it.dailyQuoteSeq,
+                korQuote = it.korQuote,
+                engQuote = it.engQuote,
+                korAuthor = it.korAuthor,
+                engAuthor = it.engAuthor,
+                authorUrl = it.authorUrl
+            )
+        }
+    }
 
 }
