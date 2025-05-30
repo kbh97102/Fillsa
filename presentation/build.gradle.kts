e496a52eeb355bda96e148c07cc232183972e708
Arakene <50691301+kbh97102@users.noreply.github.com>
2025-05-30 22:17:12 +0900

    Revert "Feature/홈화면"

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,18 +1,8 @@
-import java.util.Properties
-
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.kapt)
-    alias(libs.plugins.hilt.plugin)
-    alias(libs.plugins.kotlinx.serialization.json)
 }
-
-val secretsFile = Properties().apply {
-    load(rootProject.file("secrets.properties").inputStream())
-}
-
 
 android {
     namespace = "com.arakene.presentation"
@@ -21,9 +11,6 @@ android {
     defaultConfig {
         minSdk = 28
         targetSdk = 35
-
-        buildConfigField("String", "kakao_key", secretsFile["KAKAO_KEY"].toString())
-        buildConfigField("String", "google_key", secretsFile["GOOGLE_KEY"].toString())
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -41,10 +28,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    apply(from = "../common.gradle")
+    kotlinOptions {
+        jvmTarget = "11"
+    }
     buildFeatures {
         compose = true
-        buildConfig = true
     }
 }
 
@@ -60,7 +48,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.googleid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -68,17 +55,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.kakao.login)
-    implementation(libs.kakao.share)
-    implementation(libs.google.login)
-    implementation(libs.google.login.auth)
-    implementation(libs.googleid)
-    implementation(libs.appauth)
-    implementation(libs.authtest)
-
-    implementation(libs.hilt)
-    kapt(libs.hilt.compiler)
-    implementation(libs.hilt.navigation)
-    runtimeOnly(libs.material3)
-    implementation(libs.compose.navigation)
 }
--- presentation/src/main/java/com/arakene/presentation/util/LoginEffect.kt
+++
@@ -1,7 +0,0 @@
-package com.arakene.presentation.util
-
-sealed interface LoginEffect: Effect {
-
-    data class Move(val screen: Screens): LoginEffect
-
-}
