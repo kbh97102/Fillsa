4a861302f66e9400c6acee0db853cd5522c3772b
arakene <arakene_dev@naver.com>
2025-05-31 13:55:51 +0900

    feat: 명언 입력 배경 색상 수정

--- presentation/src/main/java/com/arakene/presentation/ui/home/TypingQuoteView.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/home/TypingQuoteView.kt
@@ -2,6 +2,7 @@ package com.arakene.presentation.ui.home
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -19,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -44,7 +46,7 @@ fun TypingQuoteView(
         mutableStateOf(LocaleType.KOR)
     }
 
-    Column {
+    Column(modifier = Modifier.background(Color.White)) {
         TypingQuoteTopSection(
             locale = localeType,
             setLocale = {
