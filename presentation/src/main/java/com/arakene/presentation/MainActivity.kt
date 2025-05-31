3839945cd76d4af4e5491b6e1762a457c47ae957
arakene <arakene_dev@naver.com>
2025-05-31 13:53:14 +0900

    feat: 화면이동 type map 수정

--- presentation/src/main/java/com/arakene/presentation/MainActivity.kt
+++ presentation/src/main/java/com/arakene/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.arakene.presentation
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,18 +10,23 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
+import com.arakene.domain.responses.DailyQuoteDto
 import com.arakene.presentation.ui.BottomNavigationBar
 import com.arakene.presentation.ui.LoginView
 import com.arakene.presentation.ui.home.HomeView
 import com.arakene.presentation.ui.home.TypingQuoteView
 import com.arakene.presentation.ui.theme.FillsaTheme
+import com.arakene.presentation.util.DailyQuoteDtoTypeMap
 import com.arakene.presentation.util.Screens
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.serialization.json.Json
+import kotlin.reflect.typeOf
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -55,7 +61,7 @@ class MainActivity : ComponentActivity() {
                     NavHost(
                         modifier = Modifier.padding(paddingValues),
                         navController = navController,
-                        startDestination = Screens.Login
+                        startDestination = Screens.Login,
                     ) {
 
                         composable<Screens.Login> {
@@ -74,7 +80,11 @@ class MainActivity : ComponentActivity() {
                             )
                         }
 
-                        composable<Screens.DailyQuote> {
+                        composable<Screens.DailyQuote>(
+                            typeMap =  mapOf(
+                                typeOf<DailyQuoteDto>() to DailyQuoteDtoTypeMap
+                            )
+                        ) {
                             val data = it.toRoute<Screens.DailyQuote>()
                             TypingQuoteView(
                                 data.dailyQuoteDto
