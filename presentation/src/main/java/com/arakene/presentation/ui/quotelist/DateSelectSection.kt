ed3db088cc5238acbfbafb4b06ca16f3779ecf3b
arakene <arakene_dev@naver.com>
2025-06-02 09:20:35 +0900

    feat: 날짜 선택 영역 추가

---
+++ presentation/src/main/java/com/arakene/presentation/ui/quotelist/DateSelectSection.kt
@@ -0,0 +1,65 @@
+package com.arakene.presentation.ui.quotelist
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.arakene.presentation.R
+import com.arakene.presentation.ui.theme.FillsaTheme
+
+@Composable
+fun DateSelectSection(
+    startDate: String,
+    endDate: String,
+    modifier: Modifier = Modifier
+) {
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colorScheme.secondary,
+                shape = MaterialTheme.shapes.small
+            )
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+
+        Image(painterResource(R.drawable.icn_calendar), contentDescription = null)
+
+        Text(
+            "$startDate - $endDate",
+            style = FillsaTheme.typography.body2,
+            color = colorResource(R.color.gray_700),
+            modifier = Modifier.padding(start = 10.dp)
+        )
+
+        Spacer(Modifier.weight(1f))
+
+        Image(painterResource(R.drawable.icn_arrow_down_black), contentDescription = null)
+
+    }
+
+}
+
+
+@Composable
+@Preview
+private fun DateSelectSectionPreview() {
+    FillsaTheme {
+        DateSelectSection(
+            startDate = "2025.01.01",
+            endDate = "2025.02.05"
+        )
+    }
+}
---
+++ presentation/src/main/java/com/arakene/presentation/ui/quotelist/QuoteListView.kt
@@ -0,0 +1,37 @@
+package com.arakene.presentation.ui.quotelist
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun QuoteListView(
+    startDate: String,
+    endDate: String
+) {
+
+    var isLike by remember {
+        mutableStateOf(false)
+    }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(horizontal = 20.dp)) {
+
+        DateSelectSection(
+            startDate = startDate,
+            endDate = endDate,
+            modifier = Modifier.padding(top = 20.dp)
+        )
+
+    }
+
+
+}
