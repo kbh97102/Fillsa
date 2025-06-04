e1d71fca19f61db9ad3038a7c2c6e74ec321cb3a
arakene <arakene_dev@naver.com>
2025-06-04 10:43:26 +0900

    feat: 화면전환 간 높이 고정

--- presentation/src/main/java/com/arakene/presentation/ui/quotelist/QuoteListItemPager.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/quotelist/QuoteListItemPager.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
@@ -13,9 +14,15 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.SubcomposeLayout
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -45,7 +52,34 @@ fun QuoteListItemPager(
         if (memo.isEmpty()) {
             PagerItem(quote)
         } else {
-            HorizontalPager(pagerState) {
+
+            var maxHeight by remember {
+                mutableIntStateOf(0)
+            }
+
+            val density = LocalDensity.current
+
+            SubcomposeLayout {
+                val quoteCompose = subcompose("QuoteCompose") {
+                    PagerItem(quote)
+                }.first().measure(it)
+
+                val memoCompose = subcompose("MemoCompose") {
+                    PagerItem(memo)
+                }.first().measure(it)
+
+                maxHeight = maxOf(quoteCompose.height, memoCompose.height)
+
+                layout(0, 0) {
+
+                }
+            }
+
+
+            HorizontalPager(
+                pagerState,
+                modifier = Modifier.height(with(density) { maxHeight.toDp() })
+            ) {
                 when (it) {
                     0 -> {
                         PagerItem(quote)
