0a5600f86e546b95f9f5d95da3a62422ac293145
arakene <arakene_dev@naver.com>
2025-06-06 14:39:32 +0900

    feat: 공지사항 있는 경우 추가

--- presentation/src/main/java/com/arakene/presentation/ui/mypage/NoticeView.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/mypage/NoticeView.kt
@@ -10,15 +10,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.arakene.presentation.R
 import com.arakene.presentation.ui.common.HeaderSection
 import com.arakene.presentation.ui.theme.FillsaTheme
+import com.arakene.presentation.viewmodel.MyPageViewModel
 
 @Composable
 fun NoticeView(
     onBackPress: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: MyPageViewModel = hiltViewModel()
 ) {
+
+    val data = viewModel.getNotice.collectAsLazyPagingItems()
 
     Column(
         modifier = modifier
@@ -32,7 +38,13 @@ fun NoticeView(
             onBackPress = onBackPress
         )
 
-        EmptyNoticeSection()
+        if (data.itemCount == 0) {
+            EmptyNoticeSection()
+        } else {
+            NoticeListSection(
+                data
+            )
+        }
 
     }
 
