c0a2e9eca288f11aa13e8e2dd4a0384d31cb7cb3
arakene <arakene_dev@naver.com>
2025-06-10 20:34:27 +0900

    feat: 알림 스위치 수정

--- presentation/src/main/java/com/arakene/presentation/ui/mypage/AlertSwitchSection.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/mypage/AlertSwitchSection.kt
@@ -1,10 +1,13 @@
 package com.arakene.presentation.ui.mypage
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
@@ -12,6 +15,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -55,8 +60,20 @@ fun AlertSwitchSection(
             onCheckedChange = setSelected,
             colors = SwitchDefaults.colors(
                 checkedTrackColor = MaterialTheme.colorScheme.tertiary,
-                uncheckedTrackColor = colorResource(R.color.gray_bd)
-            )
+                uncheckedTrackColor = colorResource(R.color.gray_bd),
+                uncheckedThumbColor = Color.White,
+                checkedThumbColor = Color.White,
+                uncheckedBorderColor = Color.Transparent,
+                checkedBorderColor = Color.Transparent
+            ),
+            thumbContent = {
+                Box(
+                    modifier = Modifier
+                        .size(14.dp)
+                        .background(Color.White)
+                        .clip(CircleShape)
+                )
+            }
         )
 
     }
@@ -66,8 +83,21 @@ fun AlertSwitchSection(
 @Preview
 @Composable
 private fun AlertSwitchSectionPreview() {
-    AlertSwitchSection(
-        selected = true,
-        setSelected = {}
-    )
+    FillsaTheme {
+        AlertSwitchSection(
+            selected = true,
+            setSelected = {}
+        )
+    }
+}
+
+@Preview(name = "false")
+@Composable
+private fun AlertSwitchSectionPreview2() {
+    FillsaTheme {
+        AlertSwitchSection(
+            selected = false,
+            setSelected = {}
+        )
+    }
 }
