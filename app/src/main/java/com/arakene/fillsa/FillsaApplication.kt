b3abc65b5a03d40fad6da362c7d3b6dd90674595
arakene <arakene_dev@naver.com>
2025-06-10 19:47:06 +0900

    refactor: 테스트 알림 제거

--- app/src/main/java/com/arakene/fillsa/FillsaApplication.kt
+++ app/src/main/java/com/arakene/fillsa/FillsaApplication.kt
@@ -2,11 +2,9 @@ package com.arakene.fillsa
 
 import android.app.Application
 import android.content.Context
-import android.util.Log
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.arakene.data.util.DailyNotificationWorker
@@ -52,29 +50,21 @@ class FillsaApplication : Application(), Configuration.Provider {
 
     private fun scheduleDailyNotification(context: Context) {
         val currentTime = LocalDateTime.now()
-        val targetTime = currentTime.withHour(16).withMinute(22).withSecond(0).withNano(0)
+        val targetTime = currentTime.withHour(9).withMinute(0).withSecond(0).withNano(0)
 
         val delay = Duration.between(currentTime, targetTime).toMillis().let {
             if (it < 0) it + 24 * 60 * 60 * 1000 else it
         }
 
-//        val workRequest = PeriodicWorkRequestBuilder<DailyNotificationWorker>(1, TimeUnit.DAYS)
-//            .setInitialDelay(delay, TimeUnit.MILLISECONDS)
-//            .build()
-
-        val testWorkRequest = OneTimeWorkRequestBuilder<DailyNotificationWorker>()
-            .setInitialDelay(10, TimeUnit.SECONDS)
+        val workRequest = PeriodicWorkRequestBuilder<DailyNotificationWorker>(1, TimeUnit.DAYS)
+            .setInitialDelay(delay, TimeUnit.MILLISECONDS)
             .build()
 
-        Log.e(">>>>", "delay $delay currentTime $currentTime targetTime $targetTime")
-
-        WorkManager.getInstance(context).enqueue(testWorkRequest)
-
-//        WorkManager.getInstance(context).enqueueUniquePeriodicWork(
-//            "DailyNotification",
-//            ExistingPeriodicWorkPolicy.UPDATE,
-//            testWorkRequest
-//        )
+        WorkManager.getInstance(context).enqueueUniquePeriodicWork(
+            "DailyNotification",
+            ExistingPeriodicWorkPolicy.UPDATE,
+            workRequest
+        )
     }
 
     override val workManagerConfiguration: Configuration
