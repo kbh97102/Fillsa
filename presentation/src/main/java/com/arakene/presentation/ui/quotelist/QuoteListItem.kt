7967b6865e1d659510c795e1bf4793b481314ff6
arakene <arakene_dev@naver.com>
2025-06-10 11:11:13 +0900

    refactor: 코드정리

--- presentation/src/main/java/com/arakene/presentation/ui/quotelist/QuoteListItem.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/quotelist/QuoteListItem.kt
@@ -10,28 +10,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil3.ImageLoader
-import coil3.compose.AsyncImage
-import coil3.network.okhttp.OkHttpNetworkFetcherFactory
 import com.arakene.domain.responses.MemberQuotesResponse
 import com.arakene.presentation.R
 import com.arakene.presentation.ui.common.CustomAsyncImage
 import com.arakene.presentation.ui.theme.FillsaTheme
-import com.arakene.presentation.util.logDebug
-import okhttp3.OkHttpClient
 
 @Composable
 fun QuoteListItem(
@@ -39,8 +30,6 @@ fun QuoteListItem(
     data: MemberQuotesResponse,
     modifier: Modifier = Modifier
 ) {
-
-    val context = LocalContext.current
 
     val quote by remember(data) {
         mutableStateOf(
