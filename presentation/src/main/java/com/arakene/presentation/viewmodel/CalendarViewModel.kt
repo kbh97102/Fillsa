7cd8ce564a02be3257e62ca80addfedc2c219941
arakene <arakene_dev@naver.com>
2025-06-12 10:25:51 +0900

    feat: 캘린더 데이터 조회 수정

--- presentation/src/main/java/com/arakene/presentation/viewmodel/CalendarViewModel.kt
+++ presentation/src/main/java/com/arakene/presentation/viewmodel/CalendarViewModel.kt
@@ -3,7 +3,12 @@ package com.arakene.presentation.viewmodel
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.viewModelScope
 import com.arakene.domain.responses.MemberMonthlyQuoteResponse
+import com.arakene.domain.responses.MemberQuotesData
+import com.arakene.domain.responses.MonthlySummaryData
 import com.arakene.domain.usecase.calendar.GetQuotesMonthlyUseCase
+import com.arakene.domain.usecase.common.GetLoginStatusUseCase
+import com.arakene.domain.usecase.db.GetLocalQuoteUseCase
+import com.arakene.domain.util.YN
 import com.arakene.presentation.util.Action
 import com.arakene.presentation.util.BaseViewModel
 import com.arakene.presentation.util.CalendarAction
@@ -11,6 +16,7 @@ import com.arakene.presentation.util.CommonEffect
 import com.arakene.presentation.util.Effect
 import com.arakene.presentation.util.Screens
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
@@ -19,7 +25,9 @@ import javax.inject.Inject
 @HiltViewModel
 class CalendarViewModel @Inject constructor(
 
-    private val getQuotesMonthlyUseCase: GetQuotesMonthlyUseCase
+    private val getQuotesMonthlyUseCase: GetQuotesMonthlyUseCase,
+    private val getLocalQuoteUseCase: GetLocalQuoteUseCase,
+    private val getLoginStatusUseCase: GetLoginStatusUseCase
 
 ) : BaseViewModel() {
 
@@ -38,7 +46,7 @@ class CalendarViewModel @Inject constructor(
     override fun handleAction(action: Action) {
         when (val calendarAction = action as CalendarAction) {
             is CalendarAction.ChangeMonth -> {
-                getQuotesMonthly(calendarAction.target.format(dateFormatter))
+                refreshData(calendarAction.target.format(dateFormatter))
             }
 
             is CalendarAction.SelectDay -> {
@@ -53,9 +61,11 @@ class CalendarViewModel @Inject constructor(
             }
 
             is CalendarAction.ClickBottomQuote -> {
-                emitEffect(CommonEffect.Move(
-                    Screens.QuoteList
-                ))
+                emitEffect(
+                    CommonEffect.Move(
+                        Screens.QuoteList
+                    )
+                )
             }
 
             else -> {
@@ -67,7 +77,7 @@ class CalendarViewModel @Inject constructor(
     override fun emitEffect(effect: Effect) {
         when (effect) {
             is CommonEffect.Refresh -> {
-                getQuotesMonthly(dateFormatter.format(LocalDate.now()))
+                refreshData(dateFormatter.format(LocalDate.now()))
             }
 
             else -> {
@@ -76,9 +86,46 @@ class CalendarViewModel @Inject constructor(
         }
     }
 
-    private fun getQuotesMonthly(yearMonth: String) = viewModelScope.launch {
+    private fun refreshData(yearMonth: String) {
+        viewModelScope.launch {
+            val isLogged = getLoginStatusUseCase().firstOrNull() ?: false
+
+            if (isLogged) {
+                getQuotesMonthly(yearMonth)
+            } else {
+                getQuotesLocal()
+            }
+
+        }
+    }
+
+    private suspend fun getQuotesMonthly(yearMonth: String) =
         getResponse(getQuotesMonthlyUseCase(yearMonth))?.let {
             data.value = it
+        }
+
+    private suspend fun getQuotesLocal() {
+        data.value = getLocalQuoteUseCase().map {
+            MemberQuotesData(
+                dailyQuoteSeq = it.dailyQuoteSeq,
+                quoteDate = it.date,
+                quote = it.korQuote,
+                author = it.korAuthor,
+                typingYnString = if (it.typing.isEmpty()) {
+                    YN.N.type
+                } else {
+                    YN.Y.type
+                },
+                likeYnString = it.likeYn
+            )
+        }.let {
+            MemberMonthlyQuoteResponse(
+                memberQuotes = it,
+                monthlySummary = MonthlySummaryData(
+                    typingCount = it.count { data -> data.typingYn == YN.Y },
+                    likeCount = it.count { data -> data.likeYn == YN.Y },
+                )
+            )
         }
     }
 
