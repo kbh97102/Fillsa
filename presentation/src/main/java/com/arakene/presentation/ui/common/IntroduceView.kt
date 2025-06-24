fcb4bfa6cc1fb5c0899a06c708b72bae53dfee47
arakene <arakene_dev@naver.com>
2025-06-24 13:31:07 +0900

    feat: 이미지 뷰 추가

--- presentation/src/main/java/com/arakene/presentation/ui/common/IntroduceView.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/common/IntroduceView.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,6 +15,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.arakene.presentation.R
 import com.arakene.presentation.ui.theme.FillsaTheme
@@ -21,11 +23,14 @@ import com.arakene.presentation.ui.theme.FillsaTheme
 @Composable
 fun IntroduceView(modifier: Modifier = Modifier) {
 
+    val pagerState = rememberPagerState { 3 }
+
     Column {
 
         // Skip
         Row(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
                 .padding(horizontal = 15.dp, vertical = 9.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
@@ -41,12 +46,22 @@ fun IntroduceView(modifier: Modifier = Modifier) {
         }
 
         // Indicator
+        IntroduceIndicatorSection(
+            currentPage = pagerState.currentPage
+        )
 
         // Images
+        IndicatorImageSection(pagerState)
 
         // Button
 
 
     }
 
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    IntroduceView()
 }
