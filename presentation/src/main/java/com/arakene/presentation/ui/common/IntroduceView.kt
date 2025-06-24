43904ec439fece61bafd71a3680a14ec98ae72fc
arakene <arakene_dev@naver.com>
2025-06-24 13:19:43 +0900

    feat: 탑바 추가

---
+++ presentation/src/main/java/com/arakene/presentation/ui/common/IntroduceView.kt
@@ -0,0 +1,52 @@
+package com.arakene.presentation.ui.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import com.arakene.presentation.R
+import com.arakene.presentation.ui.theme.FillsaTheme
+
+@Composable
+fun IntroduceView(modifier: Modifier = Modifier) {
+
+    Column {
+
+        // Skip
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(horizontal = 15.dp, vertical = 9.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Image(painter = painterResource(R.drawable.icn_arrow), contentDescription = null)
+
+            Text(
+                stringResource(R.string.skip),
+                style = FillsaTheme.typography.body3,
+                color = colorResource(R.color.gray_500),
+                textDecoration = TextDecoration.Underline
+            )
+        }
+
+        // Indicator
+
+        // Images
+
+        // Button
+
+
+    }
+
+}
