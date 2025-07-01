00025c307c8e065fed932fe48b8a35d70767d7a7
arakene <arakene_dev@naver.com>
2025-07-01 12:50:00 +0900

    fix: 리스트아이템 배경 딤처리 추가

--- presentation/src/main/java/com/arakene/presentation/ui/common/CustomAsyncImage.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/common/CustomAsyncImage.kt
@@ -1,17 +1,14 @@
 package com.arakene.presentation.ui.common
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import coil3.ImageLoader
 import coil3.compose.AsyncImage
-import coil3.network.okhttp.OkHttpNetworkFetcherFactory
 import com.arakene.presentation.R
-import okhttp3.OkHttpClient
 
 @Composable
 fun CustomAsyncImage(
@@ -19,30 +16,16 @@ fun CustomAsyncImage(
     modifier: Modifier = Modifier,
     error: Painter? = painterResource(R.drawable.img_image_background),
     contentScale: ContentScale = ContentScale.Crop,
+    colorFilter: ColorFilter? = null
 ) {
-
-    val context = LocalContext.current
-
-    val imageLoader = remember {
-        ImageLoader.Builder(context)
-            .components {
-                add(
-                    OkHttpNetworkFetcherFactory(
-                        callFactory = {
-                            OkHttpClient()
-                        }
-                    ))
-            }
-            .build()
-    }
 
     AsyncImage(
         model = imagePath,
         contentDescription = null,
         error = error,
-        imageLoader = imageLoader,
         modifier = modifier,
-        contentScale = contentScale
+        contentScale = contentScale,
+        colorFilter = colorFilter
     )
 
 }
--- presentation/src/main/java/com/arakene/presentation/ui/quotelist/QuoteListItem.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/quotelist/QuoteListItem.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -67,6 +68,7 @@ fun QuoteListItem(
             CustomAsyncImage(
                 imagePath = data.imagePath ?: "",
                 modifier = Modifier.fillMaxSize(),
+                colorFilter = ColorFilter.tint(color = colorResource(R.color.gray_700).copy(alpha = 0.3f))
             )
 
             Column(
