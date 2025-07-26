451c814f217a1efb38ba36f6009a013e965fd3b9
arakene <arakene_dev@naver.com>
2025-07-26 12:34:55 +0900

    feat: 탑영역 수정

--- presentation/src/main/java/com/arakene/presentation/ui/home/HomeTopSection.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/home/HomeTopSection.kt
@@ -2,14 +2,17 @@ package com.arakene.presentation.ui.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,7 +24,8 @@ import com.arakene.presentation.util.noEffectClickable
 @Composable
 fun HomeTopSection(
     navigate: Navigate,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    darkMode: Boolean = isSystemInDarkTheme()
 ) {
 
     Row(
@@ -33,7 +37,11 @@ fun HomeTopSection(
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Image(
-            painterResource(R.drawable.icn_logo),
+            if (darkMode) {
+                painterResource(R.drawable.icn_logo_dark)
+            } else {
+                painterResource(R.drawable.icn_logo)
+            },
             contentDescription = null,
             modifier = Modifier.noEffectClickable {
                 navigate(Screens.Home())
@@ -44,7 +52,9 @@ fun HomeTopSection(
             contentDescription = null,
             modifier = Modifier.noEffectClickable {
                 navigate(Screens.MyPage)
-            })
+            },
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onPrimary)
+        )
     }
 
 }
