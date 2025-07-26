e6186af741ffe2ab32a97d49265c443d41bd2a77
arakene <arakene_dev@naver.com>
2025-07-26 12:32:10 +0900

    feat: 버튼 색상 변경

--- presentation/src/main/java/com/arakene/presentation/ui/home/InteractionButtonSection.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/home/InteractionButtonSection.kt
@@ -1,12 +1,18 @@
 package com.arakene.presentation.ui.home
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,25 +26,40 @@ fun InteractionButtonSection(
     share: () -> Unit,
     isLike: Boolean,
     setIsLike: (Boolean) -> Unit,
-    modifier: Modifier = Modifier
-
+    modifier: Modifier = Modifier,
+    darkMode: Boolean = isSystemInDarkTheme()
 ) {
 
-    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+    val color = remember(darkMode) {
+        if (darkMode) {
+            R.color.purple01
+        } else {
+            R.color.gray_700
+        }
+    }
+
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
 
         Image(
             painter = painterResource(R.drawable.icn_copy),
             contentDescription = null,
             modifier = Modifier.noEffectClickable {
                 copy()
-            })
+            },
+            colorFilter = ColorFilter.tint(color = colorResource(color))
+        )
 
         Image(
             painter = painterResource(R.drawable.icn_share),
             contentDescription = null,
             modifier = Modifier
                 .padding(horizontal = 40.dp)
-                .noEffectClickable { share() })
+                .noEffectClickable { share() },
+            colorFilter = ColorFilter.tint(color = colorResource(color)))
 
         Image(
             painter =
@@ -49,7 +70,8 @@ fun InteractionButtonSection(
                 }, contentDescription = null,
             modifier = Modifier.noEffectClickable {
                 setIsLike(!isLike)
-            }
+            },
+            colorFilter = ColorFilter.tint(color = colorResource(color))
         )
 
     }
