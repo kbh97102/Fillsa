1d2c9d2d016ca4decc3557e284fae6f021f02485
arakene <arakene_dev@naver.com>
2025-07-26 13:13:25 +0900

    feat: 메모 입력란 수정

--- presentation/src/main/java/com/arakene/presentation/ui/common/HeaderSection.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/common/HeaderSection.kt
@@ -4,11 +4,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.arakene.presentation.R
@@ -24,17 +25,21 @@ fun HeaderSection(
 
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier.fillMaxWidth().padding(vertical = 13.dp)
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 13.dp)
     ) {
         Image(
             painter = painterResource(R.drawable.icn_arrow),
             contentDescription = null,
-            modifier = Modifier.noEffectClickable { onBackPress() })
+            modifier = Modifier.noEffectClickable { onBackPress() },
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onPrimary)
+        )
 
         Text(
             text,
             style = FillsaTheme.typography.heading4,
-            color = colorResource(R.color.gray_700),
+            color = MaterialTheme.colorScheme.onPrimary,
             modifier = Modifier.padding(start = 10.dp)
         )
     }
