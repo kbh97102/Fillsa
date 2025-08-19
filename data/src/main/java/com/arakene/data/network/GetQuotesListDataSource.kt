10938d757f997964a092d1ad4bf30ce26581b7f5
arakene <arakene_dev@naver.com>
2025-08-19 14:03:57 +0900

    fix: 페이징에서 버전오류 반응

--- data/src/main/java/com/arakene/data/network/GetQuotesListDataSource.kt
+++ data/src/main/java/com/arakene/data/network/GetQuotesListDataSource.kt
@@ -1,8 +1,10 @@
 package com.arakene.data.network
 
+import android.util.Log
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.arakene.domain.responses.MemberQuotesResponse
+import com.arakene.domain.util.AccessVersionException
 
 class GetQuotesListDataSource(
     private val api: FillsaApi,
@@ -17,6 +19,7 @@ class GetQuotesListDataSource(
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MemberQuotesResponse> {
         val page = params.key ?: 0
+        Log.e(">>>>", "여기안오니? 11")
         return try {
             val response = api.getQuoteList(
                 page = page,
@@ -24,18 +27,25 @@ class GetQuotesListDataSource(
                 likeYn = likeYn
             )
 
-            val data = response.body()?.content.orEmpty()
-            val totalPage = response.body()?.totalPages ?: 0
-            val currentPage = response.body()?.currentPage ?: 0
+            if (response.isSuccessful) {
+                val data = response.body()?.content.orEmpty()
+                val totalPage = response.body()?.totalPages ?: 0
+                val currentPage = response.body()?.currentPage ?: 0
 
-            val isLast = data.isEmpty() || totalPage - currentPage == 1
+                val isLast = data.isEmpty() || totalPage - currentPage == 1
 
-            LoadResult.Page(
-                data = data,
-                prevKey = if (page == 0) null else page - 1,
-                nextKey = if (isLast) null else page + 1
-            )
+                Log.e(">>>>", "response Code ${response.code()}")
+
+                LoadResult.Page(
+                    data = data,
+                    prevKey = if (page == 0) null else page - 1,
+                    nextKey = if (isLast) null else page + 1
+                )
+            } else {
+                LoadResult.Error(AccessVersionException())
+            }
         } catch (e: Exception) {
+            Log.e(">>>>", "여기안오니? 22 $e")
             e.printStackTrace()
             LoadResult.Error(e)
         }
