4f790acc1f571d5d5b36535aac8a493053dfe22d
arakene <arakene_dev@naver.com>
2025-09-03 19:51:21 +0900

    feat: 광고 디자인 반영

--- presentation/src/main/java/com/arakene/presentation/ui/common/SingleLineAdSection.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/common/SingleLineAdSection.kt
@@ -5,13 +5,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.LocalContentColor
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -22,7 +24,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.graphics.drawable.toBitmap
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LifecycleResumeEffect
@@ -32,7 +36,6 @@ import com.arakene.presentation.R
 import com.arakene.presentation.ui.theme.FillsaTheme
 import com.arakene.presentation.util.MyPageScreens
 import com.arakene.presentation.viewmodel.AdViewModel
-import com.google.android.gms.compose_util.NativeAdAttribution
 import com.google.android.gms.compose_util.NativeAdHeadlineView
 import com.google.android.gms.compose_util.NativeAdIconView
 import com.google.android.gms.compose_util.NativeAdView
@@ -96,16 +99,15 @@ fun SingleLineAdSection(
                         .padding(horizontal = 10.dp)
                 ) {
                     Row(
+                        modifier = Modifier.fillMaxWidth(),
                         verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
                     ) {
-                        // Display the ad attribution.
-                        NativeAdAttribution(
-                            modifier = Modifier.weight(0.2f)
-                        )
-                        // Add remaining assets such as the image and media view.
+                        AdAttributeIcon()
+
+                        Spacer(Modifier.width(6.dp))
 
                         Row(
-                            modifier = Modifier.weight(0.6f),
                             horizontalArrangement = Arrangement.Center,
                             verticalAlignment = Alignment.CenterVertically
                         ) {
@@ -126,14 +128,16 @@ fun SingleLineAdSection(
                                             text = it,
                                             color = Color.Black,
                                             textAlign = TextAlign.Center,
-                                            style = FillsaTheme.typography.buttonXSmallNormal
+                                            style = FillsaTheme.typography.buttonXSmallNormal.copy(
+                                                fontSize = 10.sp
+                                            ),
+                                            maxLines = 1,
+                                            overflow = TextOverflow.Ellipsis
                                         )
                                     }
                                 }
                             }
                         }
-
-                        Box(modifier = Modifier.weight(0.2f))
                     }
                 }
             }
@@ -142,5 +146,16 @@ fun SingleLineAdSection(
         }
 
         else -> {}
+    }
+}
+
+@Composable
+private fun AdAttributeIcon(modifier: Modifier = Modifier) {
+    Box(
+        modifier
+            .background(colorResource(R.color.gray_700), shape = RoundedCornerShape(100))
+            .padding(horizontal = 6.dp)
+    ) {
+        Text("AD", style = FillsaTheme.typography.body4, color = Color.White)
     }
 }
