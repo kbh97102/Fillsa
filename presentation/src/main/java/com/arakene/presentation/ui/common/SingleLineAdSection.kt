5d7b57f30dc737cc60bbfbf287522df5bdc974ce
arakene <arakene_dev@naver.com>
2025-09-07 12:33:19 +0900

    fix: native view 하도록 수정

--- presentation/src/main/java/com/arakene/presentation/ui/common/SingleLineAdSection.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/common/SingleLineAdSection.kt
@@ -1,17 +1,18 @@
 package com.arakene.presentation.ui.common
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -20,9 +21,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.graphics.drawable.toBitmap
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LifecycleResumeEffect
@@ -31,11 +35,12 @@ import com.arakene.domain.model.AdState
 import com.arakene.presentation.R
 import com.arakene.presentation.ui.theme.FillsaTheme
 import com.arakene.presentation.util.MyPageScreens
+import com.arakene.presentation.util.logDebug
 import com.arakene.presentation.viewmodel.AdViewModel
+import com.google.android.gms.ads.nativead.NativeAd
 import com.google.android.gms.compose_util.NativeAdAttribution
 import com.google.android.gms.compose_util.NativeAdHeadlineView
 import com.google.android.gms.compose_util.NativeAdIconView
-import com.google.android.gms.compose_util.NativeAdView
 
 @Composable
 fun SingleLineAdSection(
@@ -90,57 +95,115 @@ fun SingleLineAdSection(
             MaterialTheme(
                 colorScheme = modifiedColorScheme
             ) {
-                NativeAdView(
-                    modifier = modifier
-                        .background(color = MaterialTheme.colorScheme.primary)
-                        .padding(horizontal = 10.dp)
-                ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                    ) {
-                        // Display the ad attribution.
-                        NativeAdAttribution(
-                            modifier = Modifier.weight(0.2f)
-                        )
-                        // Add remaining assets such as the image and media view.
-
-                        Row(
-                            modifier = Modifier.weight(0.6f),
-                            horizontalArrangement = Arrangement.Center,
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            ads.icon?.let { icon ->
-                                NativeAdIconView(Modifier.padding(5.dp)) {
-                                    icon.drawable?.toBitmap()?.let { bitmap ->
-                                        Image(
-                                            bitmap = bitmap.asImageBitmap(),
-                                            "Icon",
-                                            modifier = Modifier.size(16.dp)
-                                        )
-                                    }
-                                }
-                                ads.headline?.let {
-                                    NativeAdHeadlineView {
-                                        Text(
-                                            modifier = Modifier,
-                                            text = it,
-                                            color = Color.Black,
-                                            textAlign = TextAlign.Center,
-                                            style = FillsaTheme.typography.buttonXSmallNormal
-                                        )
-                                    }
-                                }
-                            }
-                        }
-
-                        Box(modifier = Modifier.weight(0.2f))
-                    }
+                LaunchedEffect(ads) {
+                    logDebug(
+                        """
+                        extra ${ads.extras} 
+                        adChoiceInfo ${ads.adChoicesInfo}
+                        callTo Action ${ads.callToAction}
+                        store ${ads.store}
+                    """.trimIndent()
+                    )
                 }
+
+                TestNativeAd(ads, modifier = Modifier.fillMaxWidth())
+//                NativeAdView(
+//                    modifier = modifier
+//                        .background(color = MaterialTheme.colorScheme.primary)
+//                        .padding(horizontal = 10.dp)
+//                ) {
+//
+//                }
             }
 
 
         }
 
         else -> {}
+    }
+}
+
+
+@Composable
+fun TestNativeAd(nativeAd: NativeAd, modifier: Modifier = Modifier) {
+    val localContext = LocalContext.current
+    val nativeAdView = remember {
+        com.google.android.gms.ads.nativead.NativeAdView(localContext)
+            .apply { id = View.generateViewId() }
+    }
+    val composeView = remember {
+        ComposeView(localContext).apply {
+            layoutParams =
+                ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                )
+            setContent {
+
+            }
+        }
+    }
+
+    AndroidView(
+        factory = {
+            nativeAdView.apply {
+                layoutParams =
+                    ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                    )
+                addView(
+                    composeView
+                )
+            }
+        },
+        update = { view ->
+            nativeAdView.setNativeAd(nativeAd)
+            nativeAdView.callToActionView = composeView
+            composeView.setContent { TestNativeAdContent(nativeAd) }
+        },
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun TestNativeAdContent(ads: NativeAd, modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        // Display the ad attribution.
+//        NativeAdAttribution(
+//            modifier = Modifier.weight(0.2f)
+//        )
+        // Add remaining assets such as the image and media view.
+
+        Row(
+            modifier = Modifier.weight(0.6f),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+
+
+            ads.icon?.let { icon ->
+                icon.drawable?.toBitmap()?.let { bitmap ->
+                    Image(
+                        bitmap = bitmap.asImageBitmap(),
+                        "ad icon",
+                        modifier = Modifier.size(16.dp)
+                    )
+                }
+            }
+
+            ads.headline?.let {
+                Text(
+                    modifier = Modifier,
+                    text = it,
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                    style = FillsaTheme.typography.buttonXSmallNormal
+                )
+            }
+        }
     }
 }
