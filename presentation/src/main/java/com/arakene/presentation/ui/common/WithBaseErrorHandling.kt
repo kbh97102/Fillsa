a5acdcee49e602a5f3640f4abec98878a6dfd7a8
arakene <arakene_dev@naver.com>
2025-09-09 18:16:26 +0900

    fix: 제너럴 에러 스낵바로 교환

--- presentation/src/main/java/com/arakene/presentation/ui/common/WithBaseErrorHandling.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/common/WithBaseErrorHandling.kt
@@ -135,14 +135,15 @@ inline fun <reified VM : BaseViewModel> WithBaseErrorHandling(
                     }
 
                     else -> {
-                        dialogDataHolder.apply {
-                            data = DialogData.Builder()
-                                .singleButton(true)
-                                .title("요청을 처리할 수 없습니다. 잠시 후 다시 시도해 주세요.")
-                                .build()
-                        }.run {
-                            show = true
-                        }
+                        snackBar.showSnackbar("요청을 처리할 수 없습니다. 잠시 후 다시 시도해 주세요.")
+//                        dialogDataHolder.apply {
+//                            data = DialogData.Builder()
+//                                .singleButton(true)
+//                                .title("요청을 처리할 수 없습니다. 잠시 후 다시 시도해 주세요.")
+//                                .build()
+//                        }.run {
+//                            show = true
+//                        }
                     }
                 }
             }
