d95ebba037e95a2ba04bd7e8a05f8e6ba61226fc
arakene <arakene_dev@naver.com>
2025-09-12 12:13:58 +0900

    fix: 저자 텍스트 색상 수정

--- presentation/src/main/java/com/arakene/presentation/ui/common/AuthorText.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/common/AuthorText.kt
@@ -1,16 +1,13 @@
 package com.arakene.presentation.ui.common
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
-import com.arakene.presentation.R
 import com.arakene.presentation.ui.theme.FillsaTheme
 import com.arakene.presentation.util.getWikipediaUriString
 import com.arakene.presentation.util.noEffectClickable
@@ -20,7 +17,7 @@ fun AuthorText(
     author: String,
     modifier: Modifier = Modifier,
     style: TextStyle = FillsaTheme.typography.body2,
-    color: Color = MaterialTheme.colorScheme.onPrimary
+    color: Color = FillsaTheme.colorScheme.onBackground1
 ) {
 
     val uriHandler = LocalUriHandler.current
