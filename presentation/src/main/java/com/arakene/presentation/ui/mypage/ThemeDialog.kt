0e5cc7d5207b384b734d0edff4b2fdd33403176e
arakene <arakene_dev@naver.com>
2025-12-29 22:25:02 +0900

    fix: 테마 선택 18로 수정

--- presentation/src/main/java/com/arakene/presentation/ui/mypage/ThemeDialog.kt
+++ presentation/src/main/java/com/arakene/presentation/ui/mypage/ThemeDialog.kt
@@ -1,6 +1,5 @@
 package com.arakene.presentation.ui.mypage
 
-import android.view.MenuItem
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -20,20 +19,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.arakene.domain.util.DarkModeType
 import com.arakene.presentation.R
 import com.arakene.presentation.ui.common.PositiveButton
 import com.arakene.presentation.ui.theme.FillsaTheme
-import com.arakene.domain.util.DarkModeType
-import com.arakene.presentation.util.IsDarkMode
 import com.arakene.presentation.util.noEffectClickable
 
 @Composable
@@ -134,7 +130,7 @@ fun ThemeItem(
     ) {
         Text(
             text,
-            style = FillsaTheme.typography.buttonLargeNormal,
+            style = FillsaTheme.typography.buttonLargeNormal.copy(fontSize = 18.sp),
             color = FillsaTheme.colorScheme.onBackground1
         )
 
